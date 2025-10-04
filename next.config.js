4982fec56f78d328c0e33122393d82b57d40accc
sht27363838-glitch <sht27363838@gmail.com>
2025-10-05 01:18:28 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  // experimental: { typedRoutes: true }, // HOTFIX: disable (but Nav also works if it is ON)
-}
-module.exports = nextConfig
+  experimental: {
+    // 타입 엄격 라우팅 비활성화 → Link href에 문자열 사용 가능
+    typedRoutes: false
+  },
+  reactStrictMode: true,
+  trailingSlash: false
+};
+module.exports = nextConfig;
