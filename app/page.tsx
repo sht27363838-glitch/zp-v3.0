f9a219862e40b69714027ab6e3b5fbee8db038c4
sht27363838-glitch <sht27363838@gmail.com>
2025-10-05 01:11:28 +0900

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,2 +0,0 @@
-export const dynamic='force-static'
-export default function Page(){return <div className='container'><div className='card'><h3>C0 지휘소</h3><p className='badge warn'>샘플 레이아웃 정상</p></div><div className='card'>홈 화면(지휘소) 자리</div></div>}
